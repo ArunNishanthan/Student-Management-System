995103bd6911d832c9f01879ad8d7cda8b2a91b8
arunn <arunn@EDITH>
2019-12-06 09:43:08 +0800

    Main Branch

---
+++ sms/src/main/java/com/team3/sms/SmsApplication.java
@@ -0,0 +1,14 @@
+package com.team3.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class SmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsApplication.class, args);
+	}
+
+}
